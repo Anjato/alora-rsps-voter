926c6e99c01efe7bee6d3f3778632e02a3878500
Andrew <atolkinen@gmail.com>
2023-07-08 18:40:29 -0500

    check for duplicate data

--- sql_database.py
+++ sql_database.py
@@ -12,15 +12,25 @@ def get_connection_string():
 def save_data(ip, region, auth):
     connection_string = get_connection_string()
     connection = pyodbc.connect(connection_string)
-
+    print("Connecting to SQL database.")
     try:
         with connection.cursor() as cursor:
+            # Check if the auth code already exists for the given IP
+            stored_procedure = "{CALL sp_FindDuplicateAuthCodes(?, ?)}"
+            cursor.execute(stored_procedure, ip, auth)
+            duplicate_found = cursor.fetchone()[0]
+            if duplicate_found:
+                print("Duplicate IP and auth code found. Skipping data insertion.")
+                return
+
             current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             data = (ip, region, auth, current_datetime, 0)
 
             stored_procedure = "{CALL sp_InsertAuthCode(?, ?, ?, ?, ?)}"
+            print("Saving data to SQL database")
             cursor.execute(stored_procedure, data)
             connection.commit()
 
     finally:
+        print("Closing SQL connection")
         connection.close()

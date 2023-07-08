3025a0b7fdf96fdd35ebe4e5d5c0af107680dcc5
Andrew <atolkinen@gmail.com>
2023-07-07 20:25:49 -0500

    implemented recaptchav2 solver. nearly identical to hcaptcha

--- captchasolver.py
+++ captchasolver.py
@@ -1,11 +1,22 @@
 from twocaptcha import TwoCaptcha
 
 
+api_key = "fc8553a0f9973a08d7d13f83e233dc55"
+
+
 def hcaptcha_solver(sitekey, url):
-    api_key = "fc8553a0f9973a08d7d13f83e233dc55"
     solver = TwoCaptcha(api_key)
 
     response = solver.hcaptcha(sitekey=sitekey, url=url)
+    solution = response["code"]
+
+    return solution
+
+
+def recaptcha2_solver(sitekey, url):
+    solver = TwoCaptcha(api_key)
+
+    response = solver.recaptcha(sitekey=sitekey, url=url)
     solution = response["code"]
 
     return solution
